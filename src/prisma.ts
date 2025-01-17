076031690cdee8bb829e49d703161c1fee1a21e0
unknown <105841111022@student.unismuh.ac.id>
2025-01-17 11:09:30 +0800

    save

---
+++ src/prisma.ts
@@ -0,0 +1,14 @@
+import { Injectable, OnModuleInit } from "@nestjs/common";
+import { PrismaClient } from "@prisma/client";
+
+@Injectable()
+
+export default class PrismaServis extends
+PrismaClient
+implements OnModuleInit {
+    async onModuleInit() {
+        await this.$connect()
+    }
+}
+
+
